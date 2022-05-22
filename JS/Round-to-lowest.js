51518a2f4b86927411a58cf62d0405738f3b45a2
Antony Beardsmore <sider@me.com>
2022-05-22 21:58:50 +0100

    more codewars

---
+++ JS/Round-to-lowest.js
@@ -0,0 +1,7 @@
+// The Math.floor() function returns the largest integer less than or equal to a given number. 
+function litres(time) {
+    return Math.floor(0.5 * time)
+  }
+  litres(2)
+
+  // const litres = time => Math.floor(time * 0.5);
