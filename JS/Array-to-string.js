907dd703a384a6ea2ae3ccedb5d388b696986dc5
Antony Beardsmore <sider@me.com>
2022-05-23 13:08:13 +0100

    array to string sentence

---
+++ JS/Array-to-string.js
@@ -0,0 +1,5 @@
+// The join() method creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string.
+function smash (words) {
+    words.join(' ')
+ }
+ smash(["hello", "world"])
