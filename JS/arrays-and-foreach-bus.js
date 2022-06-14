d73dd4caccffdbcc767fe4a21e363ef1652b1bb9
Antony Beardsmore <sider@me.com>
2022-06-14 13:53:04 +0100

    butstop example - arrays and forEach

--- JS/arrays-and-foreach-bus.js
+++ JS/arrays-and-foreach-bus.js
@@ -16,8 +16,11 @@ var number = function (busStops) {
   return x
 }
 
+const number2 = (busStops) =>
+  busStops.reduce((rem, [on, off]) => rem + on - off, 0)
+
 console.log(
-  number([
+  number2([
     [10, 0],
     [3, 5],
     [5, 8],
